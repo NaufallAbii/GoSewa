fac59d58e4548abc288b90402f29f154d7bc0eef
naufal <naufalahmad281@gmail.com>
2025-05-15 19:29:33 +0700

    menambahkan nama dan nim

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ function App() {
   return (
     <>
       <h1>GoSewa</h1>
+      <h1>NAMA : AHMAD NAUFAL ABIYYU</h1>
+      <h1>NIM : 22.11.4963</h1><br />
+      <h1>NAMA : Cokorda Gde Lanang Pringandana</h1>
+      <h1>NIM : 22.11.4952</h1><br />
+      <h1>NAMA : JOSHUA JULIANDIKA PUTRA</h1>
+      <h1>NIM : 22.11.4941</h1>
     </>
   )
 }
